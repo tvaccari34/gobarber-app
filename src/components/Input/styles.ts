c20aac49c0d6eb2db250b1b0aad0a34956d0050a
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-09-19 19:37:55 +0100

    Included user experience with css for navigation

--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,13 +1,19 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerProps {
+    isFocused: boolean;
+    isFilled: boolean;
+}
+
+export const Container = styled.div<ContainerProps>`
 
 background: #232129;
 border-radius: 10px;
-border: 2px solid #232129;
 padding: 16px;
 width: 100%;
+
 color: #666360;
+border: 2px solid #232129;
 
 display: flex;
 align-items: center;
@@ -15,6 +21,15 @@ align-items: center;
 & + div {
         margin-top: 8px;
     }
+
+${props => props.isFocused && css`
+    color: #ff9000;
+    border-color: #ff9000;
+`}
+
+${props => props.isFilled && css`
+    color: #ff9000;
+`}
 
 input {
 
