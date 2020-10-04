1de4bb8cf858970ad380982036970f03bb041f8e
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-10-04 19:55:23 +0100

    Toast Context Created

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -5,7 +5,8 @@ import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import getValidationErrors from '../../utils/getValidationErrors';
 
-import {useAuth} from '../../hooks/AuthContext';
+import {useAuth} from '../../hooks/auth';
+import {useToast} from '../../hooks/toast';
 
 import logoImg from '../../assets/logo.svg';
 import { Container, Content, Background } from './styles';
@@ -23,6 +24,7 @@ const SignIn: React.FC = () => {
     const formRef = useRef<FormHandles>(null);
 
     const {signIn} = useAuth();
+    const { addToast } = useToast();
 
     const handleSubmit = useCallback( async (data: SignInFormData) => {
 
@@ -39,7 +41,7 @@ const SignIn: React.FC = () => {
                 abortEarly: false,
             });
 
-            signIn({
+            await signIn({
                 email: data.email,
                 password: data.password,
             });
@@ -50,8 +52,9 @@ const SignIn: React.FC = () => {
                 formRef.current?.setErrors(errors);
             }
 
+            addToast();
         }
-    }, [signIn]);
+    }, [signIn, addToast]);
 
     return (
 
