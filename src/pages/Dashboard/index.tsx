6ed41e8f097b78a1aaa0ebb7a6ead4de7480e80f
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-10-10 09:11:19 +0100

    SignOut method added on Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import React, {useCallback} from 'react';
+import { useAuth } from '../../hooks/auth';
 
-const Dashboard: React.FC = () => <h1>Dashboard</h1>
+const Dashboard: React.FC = () => {
+
+    const { signOut } = useAuth();
+
+    const handleClick = useCallback( () => {
+
+        signOut();
+
+    }, [signOut]);
+
+    return (
+        <>
+            <h1>Dashboard</h1>
+            <button onClick={handleClick}>SignOut</button>
+        </>
+    );
+}
+
 
 export default Dashboard;
