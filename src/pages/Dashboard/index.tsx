fe757e2c7ce1dbf9215dd25b73b31f623799649d
Tiago Vaccari <tiago.vaccari@outlook.com>
2020-11-20 17:51:26 +0000

    Included dashboard appointment area

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,8 +1,16 @@
 import React, {useCallback} from 'react';
 import { useAuth } from '../../hooks/auth';
-import { Container, Header, HeaderContent, Profile } from './styles';
+import {    Container,
+            Header,
+            HeaderContent,
+            Profile,
+            Content,
+            Schedule,
+            NextAppoitment,
+            Calendar
+        } from './styles';
 import logoImg from '../../assets/logo.svg';
-import { FiPower } from 'react-icons/fi';
+import { FiClock, FiPower } from 'react-icons/fi';
 
 const Dashboard: React.FC = () => {
 
@@ -34,6 +42,31 @@ const Dashboard: React.FC = () => {
                     </button>
                 </HeaderContent>
             </Header>
+
+            <Content>
+                <Schedule>
+                    <h1>Appointments</h1>
+                    <p>
+                        <span>Today</span>
+                        <span>20</span>
+                        <span>Friday</span>
+                    </p>
+                    <NextAppoitment>
+                        <strong>Next appointment</strong>
+
+                        <div>
+                            <img src="https://storage.googleapis.com/shawee-production.appspot.com/shawee/profilepictures/4dc708f2-87d6-4c96-b5fd-76208161a5e7.JPG" alt="Tiago"/>
+                            <strong>Tiago Vaccari</strong>
+                            <span>
+                                <FiClock />
+                                8:00
+                            </span>
+                        </div>
+
+                    </NextAppoitment>
+                </Schedule>
+                <Calendar />
+            </Content>
         </Container>
     );
 }
